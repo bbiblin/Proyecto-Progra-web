28c15684fe682b8a245b12ca4a154ff8e7f12d8b
CarlosEsrod2 <ca.espinozar@duocuc.cl>
2024-05-11 21:11:56 -0400

    validaciones en creación de usuario
    BOTONES DE VALIDACION, MODIFICAR LUGAR, ASPECTO, ETC.

--- Proyecto prog. web/js/validaciones.js
+++ Proyecto prog. web/js/validaciones.js
@@ -12,42 +12,36 @@ function validarcorreo() {
     }
 }
 
-function validarInicioSesion() {
+function validarCrearCuenta() {
     let user = document.getElementById("username").value;
     let pass = document.getElementById("pass").value;
-    /* 
-        username: min 5 max 20
-        password: min 8    
-    */
+    let email = document.getElementById("email").value;
+
+    if (email.search(/(@duocuc.cl)|(@gmail.com)/) === -1){
+        document.getElementById("email").style.border = "1px solid red";
+        document.getElementById("resultado").innerHTML = "<div class='alert alert-danger w-50 mx-auto text-center'>" +
+        "Correo invalido</div>";
+    }else {
+        document.getElementById("email").style.border = "1px solid lightgrey";
+        document.getElementById("resultado").innerHTML = "<div class='alert alert-success w-50 mx-auto text-center'>" +
+        "Correo Permitido</div>";
+    }
+
     if (String(user).length >= 5 && String(user).length <= 20) {
         if (String(pass).length >= 8) {
-            /* Funciono */
             document.getElementById("username").style.border = "1px solid lightgrey";
             document.getElementById("pass").style.border = "1px solid lightgrey";
-            /* Alerta */
             document.getElementById("resultado").innerHTML = "<div class='alert alert-success w-50 mx-auto text-center'>" +
                 "Acceso Concedido</div>"
         } else {
-            /* Pass error */
-            /* Borde */
             document.getElementById("pass").style.border = "1px solid red";
-            /* Alerta */
             document.getElementById("resultado").innerHTML = "<div class='alert alert-danger w-50 mx-auto text-center'>" +
                 "Contraseña debe tener minimo 8 caracteres</div>"
 
         }
     } else {
-        /* User Error */
-        /* 
-            1.- Alerta
-            2.- Label
-            3.- animaciones
-            4.- letras rojo
-            5.- borde en rojo 
-        */
-        /* Borde */
         document.getElementById("username").style.border = "1px solid red";
-        /* Alerta */
         document.getElementById("resultado").innerHTML = "<div class='alert alert-danger w-50 mx-auto text-center'>Usuario debe tener minimo 5 y maximo 20 caracteres</div>"
+        document.getElementById("pass").style.border = "1px solid red";
     }
 }
