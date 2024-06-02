0f7a37d10b6ad23166117417c44864d9d152868a
bbiblin <bele.diaz@duocuc.cl>
2024-06-01 23:38:41 -0400

    tablas base de datos
    se agregó el modelo de las tablas a models.py, se agregaron las tablas en el archivo admin.py y se creó superusuario para ver las tablas en el administrador de bd de django

--- PetsAndItems/PetsAndItemsApp/admin.py
+++ PetsAndItems/PetsAndItemsApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Usuario, Producto, Carrito, metodoPago, Orden, detalleOrden
 # Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Producto)
+admin.site.register(Carrito)
+admin.site.register(metodoPago)
+admin.site.register(Orden)
+admin.site.register(detalleOrden)
