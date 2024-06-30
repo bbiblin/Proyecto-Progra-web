ca4453baa4d01ace8dc8d41a2ba318793c88a21f
bbiblin <bele.diaz@duocuc.cl>
2024-06-30 14:18:53 -0400

    CRUD Usuarios panel de administración
    Se agregó
    -Panel de administracion en el cual se puede hacer el crud correspondiente con los usuarios
    -Carpeta FriWork donde está todo el contenido q venía con la plantilla utilizada para la interfaz del panel (de ahí los 4k+ cambios)

--- PetsAndItems/PetsAndItemsApp/views.py
+++ PetsAndItems/PetsAndItemsApp/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Usuario
 
 # Create your views here.
 def index(request):
@@ -47,7 +49,66 @@ def pag_admin(request):
     context = {
         "usuario": "",
     }
-    return render(request, "pag_admin.html", context)
+    return render(request, "indexx.html", context)
+
+def admin_add(request):
+    context = {
+            "usuario": "usuarios",
+            }
+    if request.method == 'POST':
+        if request.POST.get('nombre_completo') and request.POST.get('correo') and request.POST.get('contraseña'):
+            user = Usuario()
+            user.nombre_completo = request.POST.get('nombre_completo')
+            user.correo = request.POST.get('correo')
+            user.contraseña = request.POST.get('contraseña')
+            user.save()
+            return redirect('admin_listar')    
+    else:
+        return render(request, "crud_Admin/admin_add.html", context)
+
+
+def admin_eliminar(request):
+    context = {
+        "usuario": "",
+    }
+    if request.method == 'POST':
+        if  request.POST.get('id'):
+            id_borrar = request.POST.get('id')
+            tupla = Usuario.objects.get(id_usuario = id_borrar)
+            tupla.delete()
+            return redirect('admin_listar')
+    else:
+        users = Usuario.objects.all()
+        datos = { 'usuarios' : users }
+        return render(request, "crud_Admin/admin_eliminar.html", datos)
+
+def admin_listar(request):
+    users = Usuario.objects.all()
+    datos = { 'usuarios' : users }
+    context = {
+        "usuario": "",
+    }
+    return render(request, "crud_Admin/admin_listar.html", datos)
+
+def admin_modificar(request):
+    context = {
+        "usuario": "",
+    }
+    if request.method == 'POST':
+        if  request.POST.get('id') and request.POST.get('nombre_completo') and request.POST.get('correo') and request.POST.get('contraseña'):
+            user = Usuario()
+            user.id_usuario = request.POST.get('id')
+            user.nombre_completo = request.POST.get('nombre_completo')
+            user.correo = request.POST.get('correo')
+            user.contraseña = request.POST.get('contraseña')
+            user.save()
+            return redirect('admin_listar')    
+    else:
+        users = Usuario.objects.all()
+        datos = { 'usuarios' : users }
+        return render(request, "crud_Admin/admin_modificar.html", datos)
+
+
 
 
 
